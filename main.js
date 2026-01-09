df4eb5f75a600281cb4adb57010b153221f267df
Emelinur <emelinuryzgc@gmail.com>
2026-01-09 18:02:17 +0300

    feat: finalize calculator logic with real-time updates and improved error handling

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const billInput = document.querySelector("#bill-input");
 const btns = document.querySelectorAll(".btn");
-const btnCustom = document.querySelector(".btn-custom");
+const btnCustom = document.querySelector(".custom-tip");
 const peopleInput = document.querySelector("#peopleInput");
 const tipAmount = document.querySelector("#tip-amount");
 const totalAmount = document.querySelector("#total-amount");
@@ -10,6 +10,7 @@ const peopleError = document.querySelector("#people-error");
 let billValue = 0;
 let peopleValue = 1;
 let tipValue = 0;
+
 
 billInput.addEventListener("input", () => {
   billValue = Number(billInput.value);
@@ -22,14 +23,31 @@ peopleInput.addEventListener("input", () => {
 });
 
 function calculator() {
-  if (peopleValue <= 0) {
-    peopleError.innerHTML = "Can't be zero";
-    peopleError.style.color = "red";
+ 
+  if (!peopleValue || peopleValue<=0) {
+    peopleError.style.display = "flex";
+    peopleError.style.color = "rgb(200, 100, 100)";
+    peopleInput.style.border = "2px solid rgb(200, 100, 100)";
+    
+    tipAmount.innerText = "$0.00";
+    totalAmount.innerText = "$0.00";
+    
   } else {
+   
+    peopleError.innerHTML = ""; 
+    peopleInput.style.border = "none"; 
+    
     let tipAmountPerPerson = (billValue * tipValue) / peopleValue;
-    let totalPerPerson = billValue / peopleValue + tipAmountPerPerson;
-    tipAmount.innerText = "$" + tipAmountPerPerson.toFixed(2);
-    totalAmount.innerText = "$" + totalPerPerson.toFixed(2);
+    let totalPerPerson = (billValue / peopleValue) + tipAmountPerPerson;
+
+    if (isNaN(tipAmountPerPerson) || isNaN(totalPerPerson)) {
+        tipAmount.innerText = "$0.00";
+        totalAmount.innerText = "$0.00";
+    } else {
+        tipAmount.innerText = "$" + tipAmountPerPerson.toFixed(2);
+        totalAmount.innerText = "$" + totalPerPerson.toFixed(2);
+    }
+    
   }
 }
 
@@ -41,12 +59,11 @@ btns.forEach((item) => {
     let btnValue = parseFloat(btnText);
     let btnCal = Number(btnValue / 100);
     tipValue = btnCal;
-
     calculator();
   });
 });
 
-btnCustom.addEventListener("input", () => {
+btnCustom.addEventListener("input",() => {
   let btnCustomValue = Number(btnCustom.value);
   if (!btnCustomValue) {
     tipValue = 0;
@@ -66,5 +83,8 @@ resetBtn.addEventListener("click", () => {
   tipValue = 0;
   tipAmount.innerText = "$0.00";
   totalAmount.innerText = "$0.00";
-  document.querySelector(".btn.active")?.classList.remove("active");
+   document.querySelector(".active")?.classList.remove("active");
+   peopleError.style.display = "none";
+   peopleError.style.color = "";
+  peopleInput.style.border = "";
 });
